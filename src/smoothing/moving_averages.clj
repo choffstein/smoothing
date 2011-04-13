b16cae87147bc9de841e79d7b54a108339fecf21
Corey M. Hoffstein <corey.m.hoffstein@gmail.com>
2011-04-13 14:38:43 -0400

    Added power ability to weighted moving average

--- src/smoothing/moving_averages.clj
+++ src/smoothing/moving_averages.clj
@@ -1,15 +1,17 @@
 (ns smoothing.moving-averages
-  (:gen-class))
+  (:require [clojure.contrib.generic.math-functions :as math]))
 
 (defn- average [lst] (/ (reduce + lst) (count lst)))
 (defn simple-moving-average [window lst] (map average (partition window 1 lst)))
 
-(defn- weighted-average [lst]
+(defn- weighted-average [power lst]
   (let [indices (map (partial + 1) (range (count lst)))
-	total (reduce + indices)
+	total (reduce #(+ %1 (math/pow %2 power)) 0 indices)
 	zipped-list (map vector indices lst)]
-    (reduce #(+ (/ (* (first %2) (second %2)) total) %1) 0 zipped-list)))
-(defn weighted-moving-average [window lst] (map weighted-average (partition window 1 lst)))
+    (reduce #(+ (/ (* (second %2) (math/pow (first %2) power)) total) %1) 0 zipped-list)))
+(defn weighted-moving-average [window lst & power] (map (partial weighted-average
+							       (if (nil? power) 1 (first power)))
+						      (partition window 1 lst)))
 
 (defn- exponentially-weighted-average [lst alpha]
   (reductions #(+ (* alpha %2) (* (- 1 alpha) %1)) (first lst) (rest lst)))
