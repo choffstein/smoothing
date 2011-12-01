e60452db16fbcb098733086b8e32fe1e8d71a6b7
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-11-30 19:45:49 -0800

    Removed 1.2 refs

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject smoothing "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [org.clojure/algo.generic "0.1.0"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"
+                      :exclusions [org.clojure/clojure
+                                   org.clojure/clojure-contrib]]]
   :main smoothing.core)
